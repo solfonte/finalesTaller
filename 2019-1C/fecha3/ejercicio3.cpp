dec10fcec23b860b67bc819c35f90ea1aa294a98
solfonte <msfontenla@fi.uba.ar>
2021-07-15 17:26:41 -0300

    agrego resolucion (no compila)

---
+++ 2019-1C/fecha3/ejercicio3.cpp
@@ -0,0 +1,73 @@
+/***
+Escriba un programa que imprima por salida estándar los números entre 1 y 100, en orden ascendente.
+Se pide que los números sean contabilizados por una variable global única y que los pares sean escritos
+por un hilo mientras que los impares sean escritos por otro.
+**Contemple la correcta sincronización entre hilos y la liberación de los recursos utilizados.**
+*****/
+
+#include <iostream>
+#include <thread>
+#include <utility>
+#include <mutex>
+
+
+class NumerosProtegidos{
+
+  private:
+    std::mutex mutex;
+    int numeros[100];
+    int proximaPosicionImprimir = 0;
+  public:
+    NumerosProtegidos(){
+      // inicializo variable global unica
+      for (int i = 0; i < 100; i++){
+        this->numeros[i] = i + 1;
+      }
+    }
+    bool imprimirSiElProximoEsPar(){
+      std::lock_guard<std::mutex> lck(this->mutex);
+      if (this->proximaPosicionImprimir % 2 == 0 && this->proximaPosicionImprimir <= 100){
+        std::cout << " " << this->numeros[this->proximaPosicionImprimir] << " ";
+        this->proximaPosicionImprimir ++;
+      }
+      return (proximaPosicionImprimir > 100? true:false);
+    }
+
+    bool imprimirSiElProximoEsImpar(){
+      std::lock_guard<std::mutex> lck(this->mutex);
+      if (this->proximaPosicionImprimir % 2 == 1 && (this->proximaPosicionImprimir < 100)){
+        std::cout << " " << this->numeros[this->proximaPosicionImprimir] << " ";
+        this->proximaPosicionImprimir ++;
+      }
+      return (proximaPosicionImprimir > 100? true:false);
+    }
+};
+
+
+void imprimirNumerosPares(NumerosProtegidos& numeros){
+  bool termine = false;
+  while (!termine){
+    termine = numeros.imprimirSiElProximoEsImpar();
+  }
+}
+
+void imprimirNumerosImpares(NumerosProtegidos& numeros){
+  bool termine = false;
+  while (!termine){
+    termine = numeros.imprimirSiElProximoEsImpar();
+  }
+
+}
+
+int main(){
+
+  NumerosProtegidos numeros();
+  std::thread threadPar(imprimirNumerosPares, numeros);
+  std::thread threadImpar(imprimirNumerosImpares, numeros);
+
+  threadPar.join();
+  threadImpar.join();
+
+
+  return 0;
+}
