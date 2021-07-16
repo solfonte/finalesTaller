4b1d56eb0d15fac54adfb4372e2fd1afe5d420cf
solfonte <msfontenla@fi.uba.ar>
2021-07-16 15:19:24 -0300

    imprime 2 veces el 100 pero anda bien

--- 2019-1C/fecha3/ejercicio3.cpp
+++ 2019-1C/fecha3/ejercicio3.cpp
@@ -24,6 +24,7 @@ class NumerosProtegidos{
         this->numeros[i] = i + 1;
       }
     }
+
     bool imprimirSiElProximoEsPar(){
       std::lock_guard<std::mutex> lck(this->mutex);
       if (this->proximaPosicionImprimir % 2 == 0 && this->proximaPosicionImprimir <= 100){
@@ -44,26 +45,26 @@ class NumerosProtegidos{
 };
 
 
-void imprimirNumerosPares(NumerosProtegidos& numeros){
+void imprimirNumerosPares(NumerosProtegidos* numeros){
   bool termine = false;
   while (!termine){
-    termine = numeros.imprimirSiElProximoEsImpar();
+    termine = numeros->imprimirSiElProximoEsPar();
   }
 }
 
-void imprimirNumerosImpares(NumerosProtegidos& numeros){
+void imprimirNumerosImpares(NumerosProtegidos* numeros){
   bool termine = false;
   while (!termine){
-    termine = numeros.imprimirSiElProximoEsImpar();
+    termine = numeros->imprimirSiElProximoEsImpar();
   }
 
 }
 
 int main(){
 
-  NumerosProtegidos numeros();
-  std::thread threadPar(imprimirNumerosPares, numeros);
-  std::thread threadImpar(imprimirNumerosImpares, numeros);
+  NumerosProtegidos numeros;
+  std::thread threadPar(imprimirNumerosPares, &numeros);
+  std::thread threadImpar(imprimirNumerosImpares, &numeros);
 
   threadPar.join();
   threadImpar.join();
@@ -71,3 +72,83 @@ int main(){
 
   return 0;
 }
+
+
+/*
+
+
+#include <iostream>
+#include <thread>
+#include <utility>
+#include <mutex>
+
+
+class NumerosProtegidos{
+
+  private:
+    std::mutex mutex;
+    int numeros[100];
+    int proximaPosicionImprimir = 0;
+  public:
+    NumerosProtegidos(){
+      // inicializo variable global unica
+      for (int i = 0; i < 100; i++){
+        this->numeros[i] = i + 1;
+      }
+    }
+
+    bool imprimirSiElProximoEsPar(){
+      std::lock_guard<std::mutex> lck(this->mutex);
+      if (this->proximaPosicionImprimir % 2 == 0 && this->proximaPosicionImprimir <= 100){
+        std::cout << " " << this->numeros[this->proximaPosicionImprimir] << " ";
+        this->proximaPosicionImprimir ++;
+      }
+      return (proximaPosicionImprimir > 100? true:false);
+    }
+
+    bool imprimirSiElProximoEsImpar(){
+      std::lock_guard<std::mutex> lck(this->mutex);
+      if (this->proximaPosicionImprimir % 2 == 1 && (this->proximaPosicionImprimir < 100)){
+        std::cout << " " << this->numeros[this->proximaPosicionImprimir] << " ";
+        this->proximaPosicionImprimir ++;
+      }
+      return (proximaPosicionImprimir > 100? true:false);
+    }
+
+    void imprimirNumerosPares(){
+      bool termine = false;
+      while (!termine){
+        termine = this->imprimirSiElProximoEsImpar();
+      }
+    }
+
+    void imprimirNumerosImpares(){
+      bool termine = false;
+      while (!termine){
+        termine = this->imprimirSiElProximoEsImpar();
+      }
+    }
+
+    void operator()(bool par){
+      if (par){
+        this->imprimirNumerosPares();
+      }else{
+        this->imprimirNumerosImpares();
+      }
+    }
+
+};
+
+int main(){
+
+  NumerosProtegidos numeros();
+
+  std::thread threadPar(numeros,true);
+  std::thread threadImpar(numeros,false);
+
+  threadPar.join();
+  threadImpar.join();
+
+
+  return 0;
+}*/
