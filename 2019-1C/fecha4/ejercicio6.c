6d45143d6f20ea9d3fec9411c916988fa2ebe2ad
solfonte <msfontenla@fi.uba.ar>
2021-08-08 17:04:16 -0300

    agrego ejercicio 6 de socket server (chequeado)

--- 2019-1C/fecha4/ejercicio6.c
+++ 2019-1C/fecha4/ejercicio6.c
@@ -1,3 +1,9 @@
+/***
+Escriba un programa que reciba por línea de comandos un
+Puerto y una IP. El programa debe aceptar una unica conexión e imprimir en stdout todo lo recibido.
+Al recibir el texto ‘FIN’ debe finalizar el programa sin imprimir dicho texto.
+***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -19,16 +25,27 @@ int main(int argc,const char* argv[]){
   const char* host = argv[2];
 
   struct addrinfo hints;
-  struct addrinfo* res;
+  struct addrinfo* res, *ptr;
 
   memset(&hints,0,sizeof(struct addrinfo));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = 0;
+  hints.ai_flags = AI_PASSIVE;
 
   getaddrinfo(host,port,&hints,&res);
-  int sock_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-  connect(sock_fd,res->ai_addr,res->ai_addrlen);
+  int sock_fd = -1;
+  ptr = res;
+
+  while (ptr != NULL && sock_fd < 0){
+    sock_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    ptr = ptr->ai_next;
+  }
+
+  int val = 1;
+  setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+  bind(sock_fd, res->ai_addr,res->ai_addrlen);
+  listen(sock_fd, 1);
+  int peer = accept(sock_fd, NULL, NULL);
   freeaddrinfo(res);
 
   char buff;
@@ -37,7 +54,7 @@ int main(int argc,const char* argv[]){
   char fin[] = "FIN";
   int i = 0;
   while (!termine && !hubo_error){
-    ssize_t resultado = recv(sock_fd,&buff,sizeof(buff),0);
+    ssize_t resultado = recv(peer,&buff,sizeof(buff),0);
     if (resultado == -1){
       hubo_error = true;
     }else if (fin[i] == buff){
@@ -48,7 +65,7 @@ int main(int argc,const char* argv[]){
       i = 0;
     }
   }
-  
+
   shutdown(sock_fd,SHUT_RDWR);
   close(sock_fd);
 
